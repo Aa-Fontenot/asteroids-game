d6b5f00828109d6987152b0568a6ed075cd67406
Shakashoon <fontenot.aaron3@gmail.com>
2025-12-20 00:01:47 -0600

    working on drawing player to screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import *
 
 
 def main():
@@ -13,13 +14,25 @@ Screen height: {SCREEN_HEIGHT}
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("about to enter loop")
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0 
 
     while True:
         log_state()
-        print("loop is running")
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
+        print(dt)
+
+   
+
+
 
 
 if __name__ == "__main__":
