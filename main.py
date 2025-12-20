0b98a20ad632e337d273fd1aaf0660fcd88f8a3d
Shakashoon <fontenot.aaron3@gmail.com>
2025-12-20 00:50:46 -0600

    Got rotation working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
 from player import *
 
@@ -21,14 +21,18 @@ Screen height: {SCREEN_HEIGHT}
     dt = 0 
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
         log_state()
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
-
-        print(dt)
+        
 
    
 
