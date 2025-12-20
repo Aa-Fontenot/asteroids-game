51c755982278316fdc6963809d8db6e401f12de2
Shakashoon <fontenot.aaron3@gmail.com>
2025-12-19 23:27:37 -0600

    black screen loop running

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+def main():
+    print(f"""
+Starting Asteroids with pygame version: {pygame.version.ver}
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+""")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("about to enter loop")
+
+    while True:
+        log_state()
+        print("loop is running")
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
