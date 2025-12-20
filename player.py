b692a1e0bf93d8554b45bc88c9e4896251690b41
Shakashoon <fontenot.aaron3@gmail.com>
2025-12-20 00:59:41 -0600

    WE'RE SCHMOOVIN NOW

--- player.py
+++ player.py
@@ -21,12 +21,21 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
-        print("rotation:", self.rotation)
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        print("update checker", dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
